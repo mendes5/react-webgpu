2814e7e6aa22c508c7ae610c7cd4f99c0d2e3150
Mendes <fernando.msj006@gmail.com>
2023-05-13 15:51:01 -0700

    Adds index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,62 +3,7 @@ import Head from "next/head";
 
 import { api } from "~/utils/api";
 
-import { type FC } from "react";
-import { useWebGPUContext } from "~/webgpu/canvas";
-import { useGPUDevice } from "~/webgpu/gpu-device";
-import { useFrame } from "~/webgpu/per-frame";
-import { usePipeline, useShaderModule } from "~/webgpu/shader";
-import { immediateRenderPass, renderPass } from "~/webgpu/calls";
-import { WebGPUApp } from "~/helpers/webgpu-app";
-
-const Example: FC = () => {
-  const entireShaderApparently = useShaderModule(
-    /* wgsl */ `
-    @vertex fn vsMain(@builtin(vertex_index) vertexIndex : u32) -> @builtin(position) vec4f {
-      var pos = array<vec2f, 3>(
-        vec2f( 0.0,  0.5),  // top center
-        vec2f(-0.5, -0.5),  // bottom left
-        vec2f( 0.5, -0.5)   // bottom right
-      );
-
-      return vec4f(pos[vertexIndex], 0.0, 1.0);
-    }
-
-    @fragment fn fsMain() -> @location(0) vec4f {
-      return vec4f(1.0, 0.0, 0.0, 1.0);
-    }
-  `,
-    "our hardcoded red triangle shader"
-  );
-
-  const pipeline = usePipeline(entireShaderApparently, "Main render pipeline");
-
-  const device = useGPUDevice();
-  const context = useWebGPUContext();
-
-  useFrame(() => {
-    const renderPassDescriptor: GPURenderPassDescriptor = {
-      label: "our basic canvas renderPass",
-      colorAttachments: [
-        {
-          view: context.getCurrentTexture().createView(),
-          clearValue: [0.0, 0.0, 0.1, 1],
-          loadOp: "clear",
-          storeOp: "store",
-        },
-      ],
-    };
-
-    immediateRenderPass(device, "triangle encoder", (encoder) => {
-      renderPass(encoder, renderPassDescriptor, (pass) => {
-        pass.setPipeline(pipeline);
-        pass.draw(3);
-      });
-    });
-  });
-
-  return null;
-};
+import Link from "next/link";
 
 const Home: NextPage = () => {
   const version = api.meta.version.useQuery();
@@ -66,13 +11,21 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>WebGPU Tests</title>
+        <title>WebGPU Index</title>
         <link rel="icon" href="/favicon.svg" />
         <meta rel="app-version" content={version.data} />
       </Head>
-      <WebGPUApp fullscreen>
-        <Example />
-      </WebGPUApp>
+      <ul>
+        <li>
+          <Link href="/hello-triangle">Hello Triangle</Link>
+        </li>
+        <li>
+          <Link href="/compute">Compute Shader</Link>
+        </li>
+        <li>
+          <Link href="/interstage">Interstage Variables</Link>
+        </li>
+      </ul>
     </>
   );
 };
