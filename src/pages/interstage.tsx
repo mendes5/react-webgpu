3098a9d18ba239a9d3b617c9cb7f5ac26dbea14c
Mendes <fernando.devmines@gmail.com>
2023-05-15 21:32:06 -0300

    Fix vertex positions on interstage example

--- src/pages/interstage.tsx
+++ src/pages/interstage.tsx
@@ -103,13 +103,13 @@ const Example: FC = () => {
         @vertex fn vsMain(@builtin(vertex_index) vertexIndex : u32) -> OurVertexShaderOutput {
           var pos = array<vec2f, 3>(
             vec2f( 0.0,  0.5),  // top center
-            vec2f(-0.5, -1.5),  // bottom left
-            vec2f( 0.5, -1.5)   // bottom right
+            vec2f(-0.5, -0.5),  // bottom left
+            vec2f( 0.5, -0.5)   // bottom right
           );
           var color = array<vec4f, 3>(
-            vec4f(1, 0, 1, 1), // red
+            vec4f(1, 0, 0, 1), // red
             vec4f(0, 1, 0, 1), // green
-            vec4f(0, 1, 0, 1), // blue
+            vec4f(0, 0, 1, 1), // blue
           );
           var vsOutput: OurVertexShaderOutput;
             vsOutput.position = vec4f(pos[vertexIndex], 0.0, 1.0);
