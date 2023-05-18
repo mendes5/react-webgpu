8eacdb1e72d89bff4613eaaace626ff46532d9da
Mendes <fernando.devmines@gmail.com>
2023-05-17 21:49:37 -0300

    Enables SharedArrayBuffer

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,10 @@ const config = {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin",
           },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
         ],
       },
     ];
